d4a9a352945897b96e1e1b1cabffb6be9875de82
Liam Martens <hi@liammartens.com>
2019-01-27 15:06:43 +0100

    Fixed initial value of tm

--- include/sanitycpp/sanity_object.h
+++ include/sanitycpp/sanity_object.h
@@ -54,9 +54,9 @@ private:
     /** @var The type of the objet */
     string m_type;
     /** @var Updated at datetime */
-    tm m_updated;
+    tm m_updated = { 0, 0, 1, 0, 0, 0 };
     /** @var Created at datetime */
-    tm m_created;
+    tm m_created = { 0, 0, 1, 0, 0, 0 };
 
 public:
     #pragma region templated
